337e22cf6372fbfc67588e58c1e06323aae2c7be
MINWOO KIM <rlaalsdn110@gmail.com>
2026-01-18 00:52:20 +0900

    recover the book's calendar_scheduler app

---
+++ lib/component/schedule_bottom_sheet.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:calendar_scheduler/component/custom_text_field.dart';
+import 'package:calendar_scheduler/const/colors.dart';
+import 'package:drift/drift.dart' hide Column;
+import 'package:get_it/get_it.dart';
+import 'package:calendar_scheduler/database/drift_database.dart';
+import 'package:calendar_scheduler/model/schedule_model.dart';
+import 'package:provider/provider.dart';
+import 'package:calendar_scheduler/provider/schedule_provider.dart';
+
+class ScheduleBottomSheet extends StatefulWidget {
+  final DateTime selectedDate;
+
+  const ScheduleBottomSheet({
+    required this.selectedDate,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<ScheduleBottomSheet> createState() =>
+      _ScheduleBottomSheetState();
+}
+
+class _ScheduleBottomSheetState
+    extends State<ScheduleBottomSheet> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+
+  int? startTime;
+  int? endTime;
+  String? content;
+
+  @override
+  Widget build(BuildContext context) {
+    final bottomInset = MediaQuery.of(
+      context,
+    ).viewInsets.bottom;
+
+    return Form(
+      key: formKey,
+      child: SafeArea(
+        child: Container(
+          height:
+              MediaQuery.of(context).size.height / 2 +
+              bottomInset,
+          color: Colors.white,
+          child: Padding(
+            padding: EdgeInsets.only(
+              left: 8,
+              right: 8,
+              top: 8,
+              bottom: bottomInset,
+            ),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: CustomTextField(
+                        label: '시작 시간',
+                        isTime: true,
+                        onSaved: (String? val) {
+                          startTime = int.parse(val!);
+                        },
+                        validator: timeValidator,
+                      ),
+                    ),
+                    const SizedBox(width: 16.0),
+                    Expanded(
+                      child: CustomTextField(
+                        label: '종료 시간',
+                        isTime: true,
+                        onSaved: (String? val) {
+                          endTime = int.parse(val!);
+                        },
+                        validator: timeValidator,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 8.0),
+                Expanded(
+                  child: CustomTextField(
+                    label: '내용',
+                    isTime: false,
+                    onSaved: (String? val) {
+                      content = val;
+                    },
+                    validator: contentValidator,
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () => onSavePressed(context),
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: PRIMARY_COLOR,
+                    ),
+                    child: Text('저장'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void onSavePressed(BuildContext context) async {
+    if (formKey.currentState!.validate()) {
+      formKey.currentState!.save();
+
+      context.read<ScheduleProvider>().createSchedule(
+        schedule: ScheduleModel(
+          id: 'new_model',
+          content: content!,
+          date: widget.selectedDate,
+          startTime: startTime!,
+          endTime: endTime!,
+        ),
+      );
+
+      Navigator.of(context).pop();
+    }
+  }
+
+  String? timeValidator(String? val) {
+    if (val == null) {
+      return '값을 입력해주세요';
+    }
+
+    int? number;
+
+    try {
+      number = int.parse(val);
+    } catch (e) {
+      return '숫자를 입력해주세요';
+    }
+
+    if (number < 0 || number > 24) {
+      return '0시부터 24시 사이를 입력해주세요';
+    }
+
+    return null;
+  }
+
+  String? contentValidator(String? val) {
+    if (val == null || val.isEmpty) {
+      return '값을 입력해주세요';
+    }
+
+    return null;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:calendar_scheduler/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:calendar_scheduler/database/drift_database.dart';
+import 'package:get_it/get_it.dart';
+import 'package:calendar_scheduler/provider/schedule_provider.dart';
+import 'package:calendar_scheduler/repository/schedule_repository.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting();
+
+  final database = LocalDatabase(); // ➊ 데이터베이스 생성
+
+  final repository = ScheduleRepository();
+  final scheduleProvider =
+      ScheduleProvider(repository: repository);
+
+  GetIt.I.registerSingleton<LocalDatabase>(
+      database); // ➋ GetIt에
+
+  runApp(
+    ChangeNotifierProvider(
+      // Provider를 하위 위젯들에서 사용
+      // Drift -> Provider -> Firestore(FireBase)
+      create: (_) => scheduleProvider,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
+      ),
+    ),
+  );
+}
